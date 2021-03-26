2d99db8a83d6ca62202f2daab76ab2cba4963402
Michal Raman <raman_michael@hotmail.com>
2021-03-26 18:32:02 +0100

    added music page

--- src/components/music/Music.js
+++ src/components/music/Music.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Music.css';
 
 export default function Music() {
     return (
-        <div>
-            <h1>Test Music</h1>
+        <div className="musicVideoWrap">
+            <iframe src="https://www.youtube.com/embed/VAFRU7O1JGM?wmode=opaque&amp;enablejsapi=1" height="480" width="888" scrolling="no" frameborder="0"></iframe>
+
+            <iframe src="https://www.youtube.com/embed/tKBsp7f9PPo?wmode=opaque&amp;enablejsapi=1" height="480" width="888" scrolling="no" frameborder="0"></iframe>
+
+            <iframe src="https://www.youtube.com/embed/KcywwiCsQ-E?wmode=opaque&amp;enablejsapi=1" height="480" width="888" scrolling="no" frameborder="0"></iframe>
+
+            <iframe src="https://www.youtube.com/embed/zY7t7h6DiDc?wmode=opaque&amp;enablejsapi=1" height="480" width="888" scrolling="no" frameborder="0"></iframe>
+
+            <iframe src="https://www.youtube.com/embed/aEjOeLKqzcg?wmode=opaque&amp;enablejsapi=1" height="480" width="888" scrolling="no" frameborder="0"></iframe>
+
+            <p className="copyright">© Pete Armitage 2021</p>
         </div>
     )
 }
